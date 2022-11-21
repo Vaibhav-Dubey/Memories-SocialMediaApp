f367eb72d2a898b14ff4bb65f04a9e29a18c2485
Vaibhav <vaibhavdubey1999@gmail.com>
2022-11-21 21:54:09 +0530

    frontEnd Folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
-
+import {Container , Typography , Grow , Grid, AppBar} from '@material-ui/core';
+import memories from './images/memories.png'
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 const App = () => {
-    return(<div>
-            <h1>App</h1>
-    </div>)
+    return(
+        <Container maxwidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" height="60" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="strech" spacing="3">
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+    )
 }
 
 export default App
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Post from "./Post/Post";
+
+const Posts = () => {
+    return (
+        <>
+        <h1>Posts</h1>
+        <Post />
+        </>
+    );
+}
+
+export default Posts
