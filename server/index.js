dc253fd6a63250ba9475dc61afa13c78f484a087
Vaibhav <vaibhavdubey1999@gmail.com>
2022-11-21 14:38:52 +0530

    mongoDb connection

--- server/index.js
+++ server/index.js
@@ -9,3 +9,12 @@ app.use(bodyParser.json({limit:"30mb", extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}))
 
 app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://VaibhavAdmin:KF5AvMRoU07kgKJZ@cluster0.gobid00.mongodb.net/?retryWrites=true&w=majority'
+
+const PORT   =  process.env.PORT || 5000 ; 
+
+mongoose.connect(CONNECTION_URL ,{useNewURLParser:true,useUnifiedTopology: true}) 
+    .then(()=>app.listen(PORT , ()=> console.log('server running')))
+    .catch((error)=>console.log(error.message));
+
