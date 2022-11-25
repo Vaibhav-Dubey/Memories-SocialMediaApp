280d9094d4b53093fa3730cbfa70e9e7801d1e0a
Vaibhav <vaibhavdubey1999@gmail.com>
2022-11-25 11:41:40 +0530

    creation of a post

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -12,3 +12,12 @@ export const getPosts = () =>async(dispatch)=>{
     const action ={type: 'FETCH_ALL',payload:[]}
     dispatch(action);
 }
+
+export const createPost = (post)=> async (dispatch) => {
+    try {
+        const {data} = await api.createPost(post);
+        dispatch({type:'CREATE',payload: data})
+    } catch (error) {
+        console.log(error)
+    }
+}
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,16 +2,19 @@ import React , {useState} from "react";
 import { TextField, Button , Typography , Paper } from "@material-ui/core";
 import FileBase from 'react-file-base64';
 
+import {useDispatch} from 'react-redux';
+import {createPost} from '../../actions/posts'
 import useStyles from './styles'
 
 const Form = () => {
     const classes = useStyles();
-    
+    const dispatch = useDispatch();
     const [postData , setPostData] = useState({
         creator: '' , title:'' , message:'',tags:'',selectedFile:''
     });
-    const handleSubmit = () =>{
-        
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        dispatch(createPost(postData))
     }
     const clear = () =>{
 
