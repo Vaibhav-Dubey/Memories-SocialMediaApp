c882e392ff2b5f66175f90be37c1f1be750e8a62
Vaibhav <vaibhavdubey1999@gmail.com>
2022-11-25 11:42:21 +0530

    mongoDb URl

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({limit:"30mb", extended:true}))
 
 app.use(cors());
 app.use('/posts',postsRoutes);
-const CONNECTION_URL = 'mongodb+srv://VaibhavAdmin:KF5AvMRoU07kgKJZ@cluster0.gobid00.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = ''
 
 const PORT   =  process.env.PORT || 5000 ; 
 
